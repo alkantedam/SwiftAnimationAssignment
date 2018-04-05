da98f0aa947f7a4b734d737eeb4779b178aa1a84
alkantedam <alkantedam@gmail.com>
2018-04-05 19:50:22 +0600

    Initial Commit

---
+++ MyAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAnimation
+//
+//  Created by Alina on 4/5/18.
+//  Copyright © 2018 a2b DesignLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
